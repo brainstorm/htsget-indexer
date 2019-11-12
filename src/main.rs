70d113fae24bfa950d71692cb031a67169b26b0e
Guilherme Souza <19gui94@gmail.com>
2019-11-12 15:53:03 +0100

    Reorganize code a bit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod parquet_utils;
 mod schemas;
 
 use std::fs::File;
@@ -9,11 +10,6 @@ use parquet::record::RowAccessor;
 
 use parquet::{
     basic::{Repetition, Type as BasicType},
-    column::{
-        reader::{ColumnReader, ColumnReaderImpl},
-        writer::{ColumnWriter, ColumnWriterImpl},
-    },
-    data_type::DataType,
     file::{
         properties::WriterProperties,
         writer::{FileWriter, SerializedFileWriter},
@@ -23,73 +19,6 @@ use parquet::{
         types::{GroupTypeBuilder, PrimitiveTypeBuilder},
     },
 };
-
-fn copy_column(reader: &mut ColumnReader, writer: &mut ColumnWriter) {
-    match (reader, writer) {
-        (ColumnReader::BoolColumnReader(reader), ColumnWriter::BoolColumnWriter(writer)) => {
-            copy_column_typed(reader, writer)
-        }
-        (ColumnReader::Int32ColumnReader(reader), ColumnWriter::Int32ColumnWriter(writer)) => {
-            copy_column_typed(reader, writer)
-        }
-        (ColumnReader::Int64ColumnReader(reader), ColumnWriter::Int64ColumnWriter(writer)) => {
-            copy_column_typed(reader, writer)
-        }
-        (ColumnReader::Int96ColumnReader(reader), ColumnWriter::Int96ColumnWriter(writer)) => {
-            copy_column_typed(reader, writer)
-        }
-        (ColumnReader::FloatColumnReader(reader), ColumnWriter::FloatColumnWriter(writer)) => {
-            copy_column_typed(reader, writer)
-        }
-        (ColumnReader::DoubleColumnReader(reader), ColumnWriter::DoubleColumnWriter(writer)) => {
-            copy_column_typed(reader, writer)
-        }
-        (
-            ColumnReader::ByteArrayColumnReader(reader),
-            ColumnWriter::ByteArrayColumnWriter(writer),
-        ) => copy_column_typed(reader, writer),
-        (
-            ColumnReader::FixedLenByteArrayColumnReader(reader),
-            ColumnWriter::FixedLenByteArrayColumnWriter(writer),
-        ) => copy_column_typed(reader, writer),
-        _ => unimplemented!(),
-    }
-}
-
-fn copy_column_typed<T: DataType>(
-    reader: &mut ColumnReaderImpl<T>,
-    writer: &mut ColumnWriterImpl<T>,
-) {
-    let batch_size = 10_000;
-    let mut def_levels = vec![0; batch_size];
-    let mut rep_levels = vec![0; batch_size];
-    let mut values: Vec<T::T> = vec![Default::default(); batch_size];
-
-    dbg!("new column");
-    loop {
-        let (read_values, read_levels) = reader
-            .read_batch(
-                batch_size,
-                Some(&mut def_levels),
-                Some(&mut rep_levels),
-                &mut values,
-            )
-            .unwrap();
-
-        if read_values == 0 && read_levels == 0 {
-            break;
-        }
-
-        let num_written = writer
-            .write_batch(
-                &values[..read_values],
-                Some(&def_levels[..read_levels]),
-                Some(&rep_levels[..read_levels]),
-            )
-            .unwrap();
-        assert_eq!(read_values, num_written);
-    }
-}
 
 fn handle_parquet(in_fname: &str, out_fname: &str) {
     // Open input parquet file
@@ -139,7 +68,7 @@ fn handle_parquet(in_fname: &str, out_fname: &str) {
         for column_i in 0..group_reader.num_columns() {
             let mut column_reader = group_reader.get_column_reader(column_i).unwrap();
             let mut column_writer = group_writer.next_column().unwrap().unwrap();
-            copy_column(&mut column_reader, &mut column_writer);
+            parquet_utils::copy_column(&mut column_reader, &mut column_writer);
             group_writer.close_column(column_writer).unwrap();
         }
 
